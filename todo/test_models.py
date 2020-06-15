75353646469dd40c4587206ad32e2e76a276420e
williamhbcodeinstitute <william.h.barnes@outlook.com>
2020-06-15 15:54:36 +0000

    successful test for test_models.py

---
+++ todo/test_models.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from .forms import Item
+
+
+class testModels(TestCase):
+
+    def test_done_defaults_to_false(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertFalse(item.done)
